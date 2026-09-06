cc8a6551bab7842aa182fed865c93e652af4727e
powerxranger <aggarwalkrish28@gmail.com>
2026-09-06 21:52:45 +0530

    fix download

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -11,5 +11,7 @@ export function getRandomPrompt(prompt) {
 }
 
 export async function downloadImage(_id, photo) {
-  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+  const response = await fetch(photo);
+  const blob = await response.blob();
+  FileSaver.saveAs(blob, `download-${_id}.jpg`);
 }
