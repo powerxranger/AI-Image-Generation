83c4bc3d785b098fc62a1ba312094a60c2c78ffa
powerxranger <aggarwalkrish28@gmail.com>
2026-09-06 17:53:16 +0530

    add port and client url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import imageRoutes from './routes/imageRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || '*',
+}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
@@ -24,7 +26,8 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    const PORT= process.env.PORT || 8080;
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
